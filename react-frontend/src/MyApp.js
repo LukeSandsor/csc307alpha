2f7eb93c6af3c570a2093162c598ee4dea65ccff
LukeSandsor <63250619+LukeSandsor@users.noreply.github.com>
2022-04-08 12:29:31 -0700

    Added useEffect

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table';
 import Form from './Form';
 
@@ -16,12 +17,33 @@ function MyApp() {
     setCharacters([...characters, person]);
   }
 
-  return (
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    catch (error){
+      //not doing anything with error, just acknoledging it
+      console.log(error);
+      return false;
+    }
+  }
+
+  
+useEffect(() => {
+  fetchAll().then( result => {
+    if (result)
+      setCharacters(result);
+  });
+}, [] );
+
+return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList}/>
     </div>
   )
 }
+
 
 export default MyApp;

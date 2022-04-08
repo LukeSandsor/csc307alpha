7262721a48431841da5570ee8130e6c99bed9c30
LukeSandsor <63250619+LukeSandsor@users.noreply.github.com>
2022-04-08 12:31:54 -0700

    Added cors

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,7 +1,10 @@
 const e = require('express');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
+
+app.use(cors());
 
 app.use(express.json());
 

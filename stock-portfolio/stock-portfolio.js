aa3ba46b48e2e80355e246baa288fa2377368836
LukeSandsor <63250619+LukeSandsor@users.noreply.github.com>
2022-04-15 17:39:52 -0700

    Added more testing and functionality for Stocks using TDD

--- stock-portfolio/stock-portfolio.js
+++ stock-portfolio/stock-portfolio.js
@@ -16,6 +16,39 @@ class Portfolio{
     isEmpty() {
         return this.stockList.length === 0;
     }
+    numUniqueStocks() {
+        return this.stocks;
+    }
+    addStock(stock, sharesAdded) {
+        const index = this.stockList.findIndex(element => (element.ticker === stock));
+        if(index === -1){
+            this.stockList.push(new Stock(stock,sharesAdded));
+            this.stocks+= 1;
+        }
+        else{
+            this.stockList[index].shares+= sharesAdded;
+        }
+    }
+    removeStock(stock, sharesRemoved) {
+        const index = this.stockList.findIndex(element => (element.ticker === stock));
+        if(index !== -1){
+            if(this.stockList[index].shares < sharesRemoved)
+                throw("ShareSaleException");
+            this.stockList[index].shares-= sharesRemoved;
+            if(this.stockList[index].shares === 0)
+                this.stockList.splice(index, 1);
+        }
+        else{
+            throw("StockNotFoundException");
+        }
+    }
+    getShares(stock) {
+        const index = this.stockList.findIndex(element => (element.ticker === stock));
+        if(index !== -1)
+            return this.stockList[index].shares;
+        return 0;
+    }
+
 }
 
 
--- stock-portfolio/stock-portfolio.test.js
+++ stock-portfolio/stock-portfolio.test.js
@@ -9,3 +9,79 @@ test('Testing stock portfolio isEmpty -- success', () => {
     const portfolio = new StockFile.Portfolio();
     expect(portfolio.isEmpty()).toBeTruthy();
 });
+
+test('Unique Stocks Nums -- success', () => {
+    const target = 3;
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('GME', 5);
+    portfolio.addStock('ANET', 3);
+    portfolio.addStock('VSAT', 3);
+    const result = portfolio.numUniqueStocks();
+    expect(result).toBe(target);
+});
+
+test('Unique Stocks Nums with Duplicate -- success', () => {
+    const target = 2;
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('GME', 5);
+    portfolio.addStock('ANET', 3);
+    portfolio.addStock('GME', 3);
+    const result = portfolio.numUniqueStocks();
+    expect(result).toBe(target);
+});
+
+test('Unique Stocks Nums with Duplicate -- success', () => {
+    const target = 2;
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('GME', 5);
+    portfolio.addStock('ANET', 3);
+    portfolio.addStock('GME', 3);
+    const result = portfolio.numUniqueStocks();
+    expect(result).toBe(target);
+});
+
+test('Purchase Stocks Results -- success', () => {
+    const target = 5;
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('GME', 5);
+    const result = portfolio.getShares('GME');
+    expect(result).toBe(target);
+});
+
+test('Purchase Stocks Duplicate  -- success', () => {
+    const target = 15;
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('GME', 5);
+    portfolio.addStock('GME', 10)
+    const result = portfolio.getShares('GME');
+    expect(result).toBe(target);
+});
+
+test('Sell Stocks Resuts  -- success', () => {
+    const target = 1;
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('CHDN', 5);
+    portfolio.removeStock('CHDN', 4);
+    const result = portfolio.getShares('CHDN');
+    expect(result).toBe(target);
+});
+
+test('Check shares of unowned stock -- success', () => {
+    const target = 0;
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('ANET', 10);
+    const result = portfolio.getShares('CHDN');
+    expect(result).toBe(target);
+});
+
+test('Sell Too Many Shares -- error throw', () => {
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('FMC', 100)
+    expect(() => portfolio.removeStock('FMC', 101)).toThrow(/ShareSaleException/);
+});
+
+test('Sell unowned stock -- error throw', () => {
+    const portfolio = new StockFile.Portfolio();
+    portfolio.addStock('MIND', 100)
+    expect(() => portfolio.removeStock('CVX')).toThrow(/StockNotFoundException/);
+});
